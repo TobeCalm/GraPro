f6210be48c78c457703f46e48c9547b86e089c87
Witold <11810910@mail.sustech.edu.cn>
2022-05-11 05:54:57 +0800

    second commit

--- src/main/java/life/witold/community/controller/IndexController.java
+++ src/main/java/life/witold/community/controller/IndexController.java
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class helloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model){
+public class IndexController {
+    @GetMapping("/index")
+    public String hello(){
 
-        model.addAttribute("name",name) ;
-        return "hello";
+        return "index";
 
     }
 }
